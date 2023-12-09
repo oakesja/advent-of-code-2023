830613bb58c70213215441401a2ec1e05634272b
Jacob Oakes <jacoboakes92@gmail.com>
2023-12-09 06:39:35 -0500

    Day 9 part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -13,9 +13,17 @@ fn main() -> Result<(), std::io::Error> {
         .map(|line| parse_line(&line.unwrap()))
         .collect::<Vec<Vec<i64>>>();
 
-    let part1 = values.into_iter().map(|v| extrapolate(&v)).sum::<i64>();
-
+    let part1 = values
+        .iter()
+        .map(|v| extrapolate_for_part1(&v))
+        .sum::<i64>();
     dbg!(part1);
+
+    let part2 = values
+        .iter()
+        .map(|v| extrapolate_for_part2(&v))
+        .sum::<i64>();
+    dbg!(part2);
 
     Ok(())
 }
@@ -27,7 +35,7 @@ fn parse_line(line: &str) -> Vec<i64> {
         .collect::<Vec<i64>>();
 }
 
-fn extrapolate(history: &Vec<i64>) -> i64 {
+fn extrapolate_for_part1(history: &Vec<i64>) -> i64 {
     let mut histories = vec![history.clone()];
     let mut current = history.clone();
     while !current.iter().all(|&v| v == 0) {
@@ -40,6 +48,24 @@ fn extrapolate(history: &Vec<i64>) -> i64 {
     for i in 0..histories.len() {
         let index = histories.len() - i - 1;
         value = histories[index].last().unwrap() + value;
+    }
+
+    value
+}
+
+fn extrapolate_for_part2(history: &Vec<i64>) -> i64 {
+    let mut histories = vec![history.clone()];
+    let mut current = history.clone();
+    while !current.iter().all(|&v| v == 0) {
+        let next = find_next(&current);
+        current = next.clone();
+        histories.push(current.clone());
+    }
+
+    let mut value = 0;
+    for i in 0..histories.len() {
+        let index = histories.len() - i - 1;
+        value = histories[index].first().unwrap() - value;
     }
 
     value
