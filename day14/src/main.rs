d736c9da1e891efaa0aec2fd508e3a450e48f436
Jacob Oakes <jacoboakes92@gmail.com>
2023-12-14 07:13:14 -0500

    Day 14 part 2

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -13,6 +13,48 @@ fn main() -> Result<(), std::io::Error> {
         .map(|l| l.unwrap().chars().collect::<Vec<char>>())
         .collect::<Vec<Vec<char>>>();
 
+    let mut part1_map = map.clone();
+    tilt_north(&mut part1_map);
+
+    let part1 = part1_map
+        .iter()
+        .enumerate()
+        .map(|(i, row)| {
+            let count = row.iter().filter(|c| **c == 'O').count();
+            let score = map.len() - i;
+            count * score
+        })
+        .sum::<usize>();
+
+    dbg!(part1);
+
+    let mut part2_map = map.clone();
+    for _ in 0..100 {
+        tilt_north(&mut part2_map);
+        tilt_west(&mut part2_map);
+        tilt_south(&mut part2_map);
+        tilt_east(&mut part2_map);
+        let score = part2_map
+            .iter()
+            .enumerate()
+            .map(|(i, row)| {
+                let count = row.iter().filter(|c| **c == 'O').count();
+                let score = map.len() - i;
+                count * score
+            })
+            .sum::<usize>();
+        println!("{}", score);
+    }
+
+    /*
+       test input repeats every 8 after 2 iterations
+       999999998 mod 8 = 6
+    */
+
+    Ok(())
+}
+
+fn tilt_north(map: &mut Vec<Vec<char>>) {
     loop {
         let mut moved = 0;
         for row in 1..map.len() {
@@ -31,18 +73,67 @@ fn main() -> Result<(), std::io::Error> {
         }
         moved = 0;
     }
+}
 
-    let part1 = map
-        .iter()
-        .enumerate()
-        .map(|(i, row)| {
-            let count = row.iter().filter(|c| **c == 'O').count();
-            let score = map.len() - i;
-            count * score
-        })
-        .sum::<usize>();
+fn tilt_south(map: &mut Vec<Vec<char>>) {
+    loop {
+        let mut moved = 0;
+        for row in (0..map.len() - 1).rev() {
+            for col in 0..map[row].len() {
+                let current = map[row][col];
+                let below = map[row + 1][col];
+                if current == 'O' && below == '.' {
+                    map[row][col] = '.';
+                    map[row + 1][col] = 'O';
+                    moved += 1;
+                }
+            }
+        }
+        if moved == 0 {
+            break;
+        }
+        moved = 0;
+    }
+}
 
-    dbg!(part1);
+fn tilt_east(map: &mut Vec<Vec<char>>) {
+    loop {
+        let mut moved = 0;
+        for row in 0..map.len() {
+            for col in (0..map[row].len() - 1).rev() {
+                let current = map[row][col];
+                let east = map[row][col + 1];
+                if current == 'O' && east == '.' {
+                    map[row][col] = '.';
+                    map[row][col + 1] = 'O';
+                    moved += 1;
+                }
+            }
+        }
+        if moved == 0 {
+            break;
+        }
+        moved = 0;
+    }
+}
 
-    Ok(())
+fn tilt_west(map: &mut Vec<Vec<char>>) {
+    loop {
+        let mut moved = 0;
+        for row in 0..map.len() {
+            for col in 1..map[row].len() {
+                let current = map[row][col];
+                let west = map[row][col - 1];
+                if current == 'O' && west == '.' {
+                    map[row][col] = '.';
+                    map[row][col - 1] = 'O';
+                    moved += 1;
+                }
+            }
+        }
+        if moved == 0 {
+            break;
+        }
+        moved = 0;
+    }
 }
