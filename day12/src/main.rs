afd8e8fc92dba0aab896d763c65054357fee359e
Jacob Oakes <jacoboakes92@gmail.com>
2023-12-17 17:28:05 -0500

    Day 12 part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,11 +1,15 @@
 use rayon::{prelude::*, vec};
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
+use std::ops::Range;
 
 type Springs = (Vec<char>, Vec<usize>);
 
 fn main() -> Result<(), std::io::Error> {
+    time_graph::enable_data_collection(true);
+
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
     let file = File::open(file_path)?;
@@ -16,34 +20,20 @@ fn main() -> Result<(), std::io::Error> {
     let part1: u64 = springs
         .iter()
         .map(|(sequence, groups)| {
-            let mut total = 0;
-            find_possible_solutions(&mut sequence.clone(), &groups, 0, &mut total);
-            total
+            let mut memo = HashMap::new();
+            return find_possible_solutions(&sequence.clone(), &groups, 0, &mut memo);
         })
         .sum();
     dbg!(part1);
 
-    // let s: Vec<Springs> = springs.iter().map(unfold).collect();
-    // // let (sequence, groups) = &s[1];
-    // let (sequence, groups) = &s[5];
-
-    // let mut total = 0;
-    // let mut total2 = 0;
-    // println!("{}", sequence.iter().collect::<String>());
-    // dbg!(groups);
-    // find_possible_solutions(&mut sequence.clone(), &groups, 0, &mut total, &mut total2);
-    // dbg!(total);
-    // dbg!(total2);
-
     let part2: u64 = springs
-        .par_iter()
+        .iter()
         .map(unfold)
         .enumerate()
         .map(|(i, (sequence, groups))| {
             dbg!(i);
-            let mut total = 0;
-            find_possible_solutions(&mut sequence.clone(), &groups, 0, &mut total);
-            total
+            let mut memo = HashMap::new();
+            return find_possible_solutions(&sequence.clone(), &groups, 0, &mut memo);
         })
         .sum();
     dbg!(part2);
@@ -62,106 +52,69 @@ fn parse_line(line: String) -> Springs {
 }
 
 fn find_possible_solutions(
-    sequence: &mut Vec<char>,
-    groups: &Vec<usize>,
-    index: usize,
-    total: &mut u64,
-) -> () {
-    if index == sequence.len() {
-        if is_valid_solution(&sequence, groups) {
-            *total += 1;
-        }
-        return;
-    }
-
-    let (is_valid, groups_left, current_group_size) = check_current_sequence(sequence, groups);
-    if !is_valid {
-        return;
-    }
-
-    if sequence[index] != '?' {
-        find_possible_solutions(sequence, groups, index + 1, total);
-    } else {
-        if !groups_left.is_empty()
-            && !(current_group_size.is_some() && current_group_size.unwrap() == groups_left[0])
-        {
-            sequence[index] = '#';
-            find_possible_solutions(sequence, groups, index + 1, total);
-        }
-
-        sequence[index] = '.';
-        find_possible_solutions(sequence, groups, index + 1, total);
-        sequence[index] = '?';
-    }
-}
-
-fn is_valid_solution(sequence: &[char], groups: &[usize]) -> bool {
-    let mut group_size = 0;
-    let mut groups = groups.to_vec();
-    for c in sequence {
-        if *c == '#' {
-            group_size += 1;
-        } else if group_size > 0 {
-            if groups.is_empty() {
-                return false;
-            }
-            let expected = groups.remove(0);
-            if expected != group_size {
-                return false;
-            }
-            group_size = 0;
-        }
-    }
-    if group_size > 0 {
-        if groups.is_empty() {
-            return false;
-        }
-        let expected = groups.remove(0);
-        if expected != group_size {
-            return false;
-        }
-    }
-    return groups.is_empty();
-}
-
-fn check_current_sequence<'a>(
     sequence: &Vec<char>,
     groups: &Vec<usize>,
-) -> (bool, Vec<usize>, Option<usize>) {
-    let t1 = groups.iter().sum();
-    let t2 = sequence.iter().filter(|c| **c != '.').count();
-    if t2 < t1 {
-        return (false, vec![], None);
+    index: usize,
+    memo: &mut HashMap<(usize, usize), u64>,
+) -> u64 {
+    if groups.is_empty() {
+        return if index < sequence.len() && sequence[index..].iter().any(|c| *c == '#') {
+            0
+        } else {
+            1
+        };
     }
 
-    let mut group_size = 0;
-    let mut groups = groups.clone();
-    for c in sequence {
-        if *c == '?' {
-            let current_size = if group_size > 0 {
-                Some(group_size)
-            } else {
-                None
-            };
-            return (true, groups, current_size);
-        }
-        if groups.is_empty() && *c != '.' {
-            return (false, vec![], None);
-        }
-        if *c == '#' {
-            group_size += 1;
-            if group_size > groups[0] {
-                return (false, vec![], None);
-            }
-        } else if group_size > 0 {
-            let expected = groups.remove(0);
-            if expected != group_size {
-                return (false, vec![], None);
-            }
-            group_size = 0;
+    let mut index = index;
+    while index < sequence.len() {
+        match sequence.iter().nth(index).unwrap() {
+            '#' | '?' => break,
+            _ => index += 1,
         }
     }
-    return (true, groups, None);
+
+    if index >= sequence.len() {
+        return 0;
+    }
+
+    let cached = memo.get(&(index, groups.len()));
+    if cached.is_some() {
+        return *cached.unwrap();
+    }
+
+    let first_group = groups[0];
+    let mut result = 0;
+
+    if can_fit(sequence, index..index + first_group) {
+        result += find_possible_solutions(
+            sequence,
+            &groups[1..].to_vec(),
+            index + first_group + 1,
+            memo,
+        );
+    }
+
+    if sequence[index] == '?' {
+        result += find_possible_solutions(sequence, &groups, index + 1, memo);
+    }
+
+    memo.insert((index, groups.len()), result);
+
+    return result;
+}
+
+fn can_fit(sequence: &Vec<char>, range: Range<usize>) -> bool {
+    if range.end > sequence.len() {
+        return false;
+    }
+    if sequence[range.clone()].iter().any(|x| *x == '.') {
+        return false;
+    }
+    let next = range.end;
+    if next < sequence.len() && sequence[next] == '#' {
+        return false;
+    }
+    true
 }
 
 fn unfold(springs: &Springs) -> Springs {
