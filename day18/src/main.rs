c541639b541e2ef4230ec3a2e1e3f16c87db3c76
Jacob Oakes <jacoboakes92@gmail.com>
2023-12-18 13:38:27 -0500

    Day 18 part 2

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -11,7 +10,7 @@ enum Direction {
     Right,
 }
 
-type Dig = (Direction, i32);
+type Dig = (Direction, u128);
 
 fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = env::args().collect();
@@ -24,22 +23,16 @@ fn main() -> Result<(), std::io::Error> {
         .map(|line| parse_line(line.unwrap()))
         .collect::<Vec<Dig>>();
 
-    let dig_map = build_dig_map(&dig_instructions);
-    let mut area_map = dig_map_to_area_map(dig_map);
-    for row in &area_map {
-        println!("{}", row.iter().collect::<String>());
-    }
-    flood(&mut area_map);
-    for row in &area_map {
-        println!("{}", row.iter().collect::<String>());
-    }
-
-    let part1 = area_map
-        .iter()
-        .map(|row| row.iter().filter(|c| **c != '0').count())
-        .sum::<usize>();
-
+    let part1 = find_dig_area(&dig_instructions);
     dbg!(part1);
+
+    let file = File::open(file_path)?;
+    let dig_instructions = io::BufReader::new(file)
+        .lines()
+        .map(|line| parse_line_part_2(line.unwrap()))
+        .collect::<Vec<Dig>>();
+    let part2 = find_dig_area(&dig_instructions);
+    dbg!(part2);
 
     Ok(())
 }
@@ -53,88 +46,54 @@ fn parse_line(input: String) -> Dig {
         "R" => Direction::Right,
         _ => panic!("Invalid direction"),
     };
-    let amount = parsed[1].parse::<i32>().unwrap();
+    let amount = parsed[1].parse::<u128>().unwrap();
     (direction, amount)
 }
 
-fn build_dig_map(digs: &Vec<Dig>) -> HashMap<(i32, i32), bool> {
-    let mut map = HashMap::new();
-    let mut position = (0, 0);
-    map.insert(position, true);
+fn parse_line_part_2(input: String) -> Dig {
+    let parsed = input.split(" ").collect::<Vec<&str>>();
+    let value = parsed[2];
+    let hex = value
+        .chars()
+        .skip(2)
+        .take(value.len() - 4)
+        .collect::<String>();
+    let amount = u128::from_str_radix(&hex, 16).unwrap();
+    let raw_direction = value.chars().collect::<Vec<char>>()[value.len() - 2];
+    let direction = match raw_direction {
+        '3' => Direction::Up,
+        '1' => Direction::Down,
+        '2' => Direction::Left,
+        '0' => Direction::Right,
+        _ => panic!("Invalid direction"),
+    };
+    (direction, amount)
+}
+
+fn find_dig_area(digs: &Vec<Dig>) -> u128 {
+    let mut position: (i64, i64) = (0, 0);
+    let mut perimeter = 0;
+    let mut accl = 0;
 
     for dig in digs {
+        perimeter += dig.1;
+        let mut new_position = position.clone();
         for _ in 0..dig.1 {
             match dig.0 {
-                Direction::Up => position.1 -= 1,
-                Direction::Down => position.1 += 1,
-                Direction::Left => position.0 -= 1,
-                Direction::Right => position.0 += 1,
-            }
-            map.insert(position, true);
-        }
-    }
-
-    map
-}
-
-fn dig_map_to_area_map(dig_map: HashMap<(i32, i32), bool>) -> Vec<Vec<char>> {
-    let xs = dig_map.keys().map(|(x, _)| x);
-    let ys = dig_map.keys().map(|(_, y)| y);
-    let max_x = *xs.clone().max().unwrap() + 1;
-    let min_x = *xs.clone().min().unwrap() - 1;
-    let max_y = *ys.clone().max().unwrap() + 1;
-    let min_y = *ys.clone().min().unwrap() - 1;
-
-    let mut map = vec![];
-    for y in min_y..=max_y {
-        let mut row = vec![];
-        for x in min_x..=max_x {
-            let position = (x, y);
-            if dig_map.contains_key(&position) {
-                row.push('#');
-            } else {
-                row.push('.')
+                Direction::Up => new_position.1 -= 1,
+                Direction::Down => new_position.1 += 1,
+                Direction::Left => new_position.0 -= 1,
+                Direction::Right => new_position.0 += 1,
             }
         }
-        map.push(row);
+        accl += cross(position, new_position);
+        position = new_position
     }
 
-    return map;
+    let inner_area = (accl.abs() as u128 / 2) - perimeter / 2 + 1;
+    perimeter + inner_area
 }
 
-fn flood(map: &mut Vec<Vec<char>>) {
-    let max_x = map[0].len();
-    let max_y = map.len();
-    let mut visited = HashMap::new();
-    let mut to_visit = vec![
-        (0, 0),
-        (max_x - 1, 0),
-        (0, max_y - 1),
-        (max_x - 1, max_y - 1),
-    ];
-
-    while !to_visit.is_empty() {
-        let position = to_visit.pop().unwrap();
-        if visited.contains_key(&position) {
-            continue;
-        }
-        visited.insert(position, true);
-        let (x, y) = position;
-        if x > 0 && map[y][x - 1] == '.' {
-            map[y][x - 1] = '0';
-            to_visit.push((x - 1, y));
-        }
-        if x < max_x - 1 && map[y][x + 1] == '.' {
-            map[y][x + 1] = '0';
-            to_visit.push((x + 1, y));
-        }
-        if y > 0 && map[y - 1][x] == '.' {
-            map[y - 1][x] = '0';
-            to_visit.push((x, y - 1));
-        }
-        if y < max_y - 1 && map[y + 1][x] == '.' {
-            map[y + 1][x] = '0';
-            to_visit.push((x, y + 1));
-        }
-    }
+fn cross(a: (i64, i64), b: (i64, i64)) -> i64 {
+    a.0 * b.1 - a.1 * b.0
 }
